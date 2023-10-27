2217fb6b39bd7ebe9f1a2cdc25cdaec344231dad
PavloAvd <pavlo.avd@gmail.com>
2023-10-28 02:13:49 +0300

    https fixes

--- framework/Application.js
+++ framework/Application.js
@@ -1,67 +1,3 @@
-const https = require('https');
-const fs = require('fs');
-
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/83-229-85-22.cloud-xip.com/fullchain.pem'),       // Укажите путь к вашему SSL-ключу
-  cert: fs.readFileSync('/etc/letsencrypt/live/83-229-85-22.cloud-xip.com/privkey.pem') // Укажите путь к вашему SSL-сертификату
-};
-
-class Application {
-  constructor() {
-    this.server = this._createServer();
-    this.middlewares = [];
-  }
-
-  use(middleware) {
-    this.middlewares.push(middleware);
-  }
-
-  listen(port, callback) {
-    this.server.listen(port, callback);
-  }
-
-  // Другие методы вашего сервера
-
-  _createServer() {
-    return https.createServer(options, (req, res) => {
-      let body = "";
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-      req.on('data', (chunk) => {
-        body += chunk;
-      });
-
-      req.on('end', () => {
-        if (body) {
-          req.body = JSON.parse(body);
-        }
-        this.middlewares.forEach(middleware => middleware(req, res));
-        // Обработка запросов
-      });
-    });
-  }
-}
-
-const app = new Application();
-
-// Добавьте ваши маршруты и обработчики запросов здесь, используя методы класса Application
-
-app.listen(443, () => {
-  console.log('Сервер запущен на порту 443 (HTTPS)');
-});
-
-
-
-
-
-
-
-
-
-
-
 // const http = require('http')
 // const EventEmitter = require('events');
 
@@ -120,3 +56,74 @@ app.listen(443, () => {
 //     }
 
 // }
+
+//======================NEW HTPPS 
+
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/83-229-85-22.cloud-xip.com/privkey.pem'),       // Укажите путь к вашему SSL-ключу
+  cert: fs.readFileSync('/etc/letsencrypt/live/83-229-85-22.cloud-xip.com/fullchain.pem') // Укажите путь к вашему SSL-сертификату
+};
+
+class Application {
+    constructor() {
+        this.server = this._createServer();
+        this.middlewares = [];
+    }
+    use(middleware) {
+        this.middlewares.push(middleware);
+    }
+    listen(port, callback) {
+        this.server.listen(port, callback);
+    }
+    addRouter(router) {
+        Object.keys(router.endpoints).forEach(path => {
+            const endpoint = router.endpoints[path];
+            Object.keys(endpoint).forEach((method) => {
+                this.emitter.on(this._getRouteMask(path, method), (req, res) => {
+                    const handler = endpoint[method];
+
+                    handler(req, res);
+                });
+            });
+
+        });
+    }
+    _createServer() {
+        return https.createServer(options, (req, res) => {
+            let body = "";
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+            req.on('data', (chunk) => {
+                body += chunk;
+            });
+
+            req.on('end', () => {
+                if (body) {
+                    req.body = JSON.parse(body);
+                }
+                this.middlewares.forEach(middleware => middleware(req, res));
+                const emmitted = this.emitter.emit(this._getRouteMask(req.url, req.method), req, res);
+                if (!emmitted) {
+                    res.end();
+                }
+            });
+        });
+    }
+
+    _getRouteMask(path, method) {
+        return `[${path}]:[${method}]`;
+    }
+}
+
+const app = new Application();
+
+// Добавьте ваши маршруты и обработчики запросов здесь, используя методы класса Application
+
+app.listen(443, () => {
+    console.log('Сервер запущен на порту 443 (HTTPS)');
+});
--- index.js
+++ index.js
@@ -1,23 +1,58 @@
-require('dotenv').config()
-const Application = require('./framework/Application')
-const requestRouter = require('./src/request-router')
-const jsonParser = require('./framework/parseJson')
-const parseUrl = require('./framework/parseUrl')
+// require('dotenv').config()
+// const Application = require('./framework/Application')
+// const requestRouter = require('./src/request-router')
+// const jsonParser = require('./framework/parseJson')
+// const parseUrl = require('./framework/parseUrl')
 
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 
-const app = new Application()
+// const app = new Application()
 
-app.use(jsonParser)
-app.use(parseUrl('http://localhost:5000'))
-app.addRouter(requestRouter)
+// app.use(jsonParser)
+// app.use(parseUrl('http://localhost:5000'))
+// app.addRouter(requestRouter)
+
+// const start = async () => {
+//     try {
+//         app.listen(PORT, () => console.log(`SERVER WORKING ON PORT: ${PORT}`))
+//     } catch (error) {
+//     console.log("server error", error); 
+//     }
+// }
+
+// start()
+
+
+//=========================new HTTPS
+
+require('dotenv').config();
+const fs = require('fs');
+const https = require('https');
+const Application = require('./framework/Application');
+const requestRouter = require('./src/request-router');
+const jsonParser = require('./framework/parseJson');
+const parseUrl = require('./framework/parseUrl');
+
+const PORT = process.env.PORT || 5000;
+
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/83-229-85-22.cloud-xip.com/privkey.pem'),       // Укажите путь к вашему SSL-ключу
+  cert: fs.readFileSync('/etc/letsencrypt/live/83-229-85-22.cloud-xip.com/fullchain.pem') // Укажите путь к вашему SSL-сертификату
+};
+
+const app = new Application();
+
+app.use(jsonParser);
+app.use(parseUrl('https://83-229-85-22.cloud-xip.com:5000'));
+app.addRouter(requestRouter);
 
 const start = async () => {
     try {
-        app.listen(PORT, () => console.log(`SERVER WORKING ON PORT: ${PORT}`))
+        const server = https.createServer(options, app._createServer()); // Создаем HTTPS-сервер
+        server.listen(PORT, () => console.log(`SERVER WORKING ON PORT: ${PORT}`));
     } catch (error) {
-    console.log("server error", error); 
+        console.log("server error", error);
     }
-}
+};
 
-start()
+start();
